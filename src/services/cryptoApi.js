d25734106c5b271b704947366db842d6c41d9911
= <=>
2022-07-11 16:41:12 +0800

    config home & cryptocurrencies page

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,34 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "X-RapidAPI-Key": "f5c14abe2cmshb1d2d4038bc3c9ep1676fajsn77fd71c56f9c",
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com/";
+
+const params = {
+  referenceCurrencyUuid: "yhjMzLPhuIDl",
+  timePeriod: "24h",
+  "tiers[0]": "1",
+  orderBy: "marketCap",
+  orderDirection: "desc",
+  limit: "50",
+  offset: "0",
+};
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+  }),
+});
+
+// a hook provided by redux toolkit to get the data for query
+// must have use in front and Query at the back
+export const { useGetCryptosQuery } = cryptoApi;
