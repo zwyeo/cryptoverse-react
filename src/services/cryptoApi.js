6a046d92d492d8919bf730ea475c2e180ed30d47
= <=>
2022-07-12 13:43:00 +0800

    edited files for deployment

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -7,16 +7,6 @@ const cryptoApiHeaders = {
 
 const baseUrl = "https://coinranking1.p.rapidapi.com/";
 
-const params = {
-  referenceCurrencyUuid: "yhjMzLPhuIDl",
-  timePeriod: "24h",
-  "tiers[0]": "1",
-  orderBy: "marketCap",
-  orderDirection: "desc",
-  limit: "50",
-  offset: "0",
-};
-
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
 export const cryptoApi = createApi({
